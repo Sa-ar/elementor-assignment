521902ba92d95eac40141e26d06b37aba42fe561
Saar Davidson <davidsonsaar@gmail.com>
2023-07-22 01:30:35 +0300

    add title

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import Card from "./components/Card";
 
 const queryClient = new QueryClient();
 
+const MAX_ITEMS = 5;
+
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <div className="grid min-h-screen place-items-center">
-        <Card />
+        <Card maxItems={MAX_ITEMS} />
       </div>
     </QueryClientProvider>
   );
--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,10 +1,21 @@
 import { Progress } from "./ui/Progress";
 
-function Title() {
+interface TitleProps {
+  total: number;
+  completed: number;
+}
+
+function Title({ total, completed }: TitleProps) {
   return (
-    <header className="relative px-2 py-3 bg-primary-200">
-      <h3 className="inline text-lg font-semibold">Title</h3>Title
-      <Progress value={50} className="absolute bottom-0 left-0 right-0" />
+    <header className="relative px-3 py-4 bg-primary-200">
+      <h3 className="inline text-lg font-semibold">Let's set up your site</h3>
+      <span className="inline ml-2 text-xs italic font-light">
+        {completed} of {total} complete
+      </span>
+      <Progress
+        value={total / completed}
+        className="absolute bottom-0 left-0 right-0"
+      />
     </header>
   );
 }
